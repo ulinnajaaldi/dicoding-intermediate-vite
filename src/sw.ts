6260d43c9158eaef52df49f2184f4be133c7b63f
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2025-05-14 16:49:10 +0700

    feat: update dependencies and enhance service worker caching strategies

--- src/sw.ts
+++ src/sw.ts
@@ -2,8 +2,89 @@
 declare const self: ServiceWorkerGlobalScope;
 
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { BASE_URL } from './constants/config';
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({ url }) => {
+    return (
+      url.origin === 'https://fonts.googleapis.com' || url.origin === 'https://fonts.gstatic.com'
+    );
+  },
+  new CacheFirst({
+    cacheName: 'google-fonts',
+  }),
+);
+
+registerRoute(
+  ({ url }) => {
+    return url.origin === 'https://cdnjs.cloudflare.com' || url.origin.includes('fontawesome');
+  },
+  new CacheFirst({
+    cacheName: 'fontawesome',
+  }),
+);
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(BASE_URL);
+    return baseUrl.origin === url.origin && request.destination !== 'image';
+  },
+  new NetworkFirst({
+    cacheName: 'story-api',
+  }),
+);
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(BASE_URL);
+    return baseUrl.origin === url.origin && request.destination === 'image';
+  },
+  new StaleWhileRevalidate({
+    cacheName: 'story-api-images',
+  }),
+);
+
+registerRoute(
+  ({ url }) => {
+    return url.origin.includes('maptiler');
+  },
+  new CacheFirst({
+    cacheName: 'maptiler-api',
+  }),
+);
+
+registerRoute(
+  ({ url }) => {
+    return url.origin.includes('tile.openstreetmap.org');
+  },
+  new CacheFirst({
+    cacheName: 'leaflet-maps',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  ({ request }) => {
+    return request.destination === 'script' || request.destination === 'style';
+  },
+  new StaleWhileRevalidate({
+    cacheName: 'js-css',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
 
 self.addEventListener('push', (event) => {
   console.log('[Service worker] pushing...');
